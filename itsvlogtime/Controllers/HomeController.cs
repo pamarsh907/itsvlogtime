394efc475fc5d78a57b6d7b053a38cc93f06681b
pamarsh907 <77184237+pamarsh907@users.noreply.github.com>
2021-02-22 13:06:50 -0900

    Updated youtube page

--- itsvlogtime/Controllers/HomeController.cs
+++ itsvlogtime/Controllers/HomeController.cs
@@ -11,22 +11,42 @@ namespace itsvlogtime.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+         public HomeController(ILogger<HomeController> logger)
+         {
             _logger = logger;
-        }
+         }
 
-        public IActionResult Index()
-        {
+         public IActionResult Index()
+         {
             return View();
-        }
+         }
 
-        public IActionResult Privacy()
-        {
+         public IActionResult Blog()
+		   {
             return View();
-        }
+		   }
+
+         public IActionResult Extras()
+         {
+            return View();
+         }
+
+         public IActionResult Map()
+         {
+            return View();
+         }
+
+         public IActionResult Youtube()
+         {
+            return View();
+         }
+
+         public IActionResult Privacy()
+         {
+            return View();
+         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
